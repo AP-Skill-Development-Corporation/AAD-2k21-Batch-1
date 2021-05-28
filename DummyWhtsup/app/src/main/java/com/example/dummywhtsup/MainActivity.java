7e7e5de94fc629ba4ba2557284f62cc069aa6626
muneiah tellakula <muneiahtellakula@gmail.com>
2021-05-28 17:44:37 +0530

    Tab Navigation Fragment added

--- DummyWhtsup/app/src/main/java/com/example/dummywhtsup/MainActivity.java
+++ DummyWhtsup/app/src/main/java/com/example/dummywhtsup/MainActivity.java
@@ -1,14 +1,60 @@
 package com.example.dummywhtsup;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.tabs.TabLayout;
 
+public class MainActivity extends AppCompatActivity {
+TabLayout tab;
+ViewPager viewPage;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tab=findViewById(R.id.mytab);
+        viewPage=findViewById(R.id.myViewPager);
+        viewPage.setAdapter(new MyFragementPagerAdapter(getSupportFragmentManager()));
+        tab.setupWithViewPager(viewPage);
+    }
+    public class MyFragementPagerAdapter extends FragmentPagerAdapter{
+        String titles[]={"Chats","Status","Calls"};
+        public MyFragementPagerAdapter(@NonNull FragmentManager fm) {
+            super(fm);
+        }
+
+        @Nullable
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return titles[position];
+        }
+
+        @NonNull
+        @Override
+        public Fragment getItem(int position) {
+            if (position==0){
+                return new ChatsFragment();
+            }
+            if (position==1){
+                return new StatusFragment();
+            }
+            if (position==2){
+                return  new CallsFragment();
+            }
+            return null;
+        }
+
+        @Override
+        public int getCount() {
+            return titles.length;
+        }
+        /*if you get the error then tap the alt+enter on the redline*/
     }
 }
