84aecf3b5acd5e84f8811cfd76002b438c70e740
muneiah tellakula <muneiahtellakula@gmail.com>
2021-06-04 17:08:35 +0530

    roomdb first

---
+++ MyRoomDatabase/app/src/main/java/com/example/myroomdatabase/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.myroomdatabase;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void saveData(View view) {
+    }
+
+    public void retriveData(View view) {
+    }
+}
