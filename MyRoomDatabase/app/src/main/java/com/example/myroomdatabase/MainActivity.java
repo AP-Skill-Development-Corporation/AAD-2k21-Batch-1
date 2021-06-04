add08c1f9890cdce7c4eae45f5c946adde446b25
muneiah tellakula <muneiahtellakula@gmail.com>
2021-06-04 17:32:41 +0530

    roomdb first

--- MyRoomDatabase/app/src/main/java/com/example/myroomdatabase/MainActivity.java
+++ MyRoomDatabase/app/src/main/java/com/example/myroomdatabase/MainActivity.java
@@ -1,19 +1,23 @@
 package com.example.myroomdatabase;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.myroomdatabase.databinding.ActivityMainBinding;
 
+public class MainActivity extends AppCompatActivity {
+ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+       binding= DataBindingUtil.setContentView(this,R.layout.activity_main);
     }
 
     public void saveData(View view) {
+
     }
 
     public void retriveData(View view) {
