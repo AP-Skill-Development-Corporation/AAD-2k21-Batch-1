5ae5462182a8be9c05c42535c31bbbfd3e444e31
muneiah tellakula <muneiahtellakula@gmail.com>
2021-06-05 16:41:38 +0530

    roomdb update

---
+++ MyRoomDatabase/app/src/main/java/com/example/myroomdatabase/UpdateActivity.java
@@ -0,0 +1,18 @@
+package com.example.myroomdatabase;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class UpdateActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_update);
+    }
+
+    public void updateData(View view) {
+    }
+}
